fcc05616462cdeec8b9ad3483e740d1ecb434e3b
DoctorScript <serhyy.kovalenko@i.ua>
2025-11-13 21:26:25 +0200

    +

--- gtplatform/ResourceLoader.cpp
+++ gtplatform/ResourceLoader.cpp
@@ -15,7 +15,7 @@ namespace gt
 {
     ResourceLoader::ResourceLoader(Platform&) : _prefix(":/resources/") { }
 
-    OpResult ResourceLoader::load(ResourceManager&, Texture::Builder& builder)
+    OpResult ResourceLoader::load(Texture::BaseBuilder& builder)
     {
         auto img = QImage(_prefix + builder.name());
         const uchar* data = img.constBits();
@@ -30,12 +30,12 @@ namespace gt
         return OpResult::OK;
     }
 
-    OpResult ResourceLoader::load(ResourceManager& manager, Font::Builder& builder)
+    OpResult ResourceLoader::load(Font::BaseBuilder& builder)
     {
         QFile file(_prefix + builder.name());
         if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
         {
-            builder.failed(manager);
+            builder.failed();
             FAIL_OP("Font '%s' not found", builder.name());
         }
 
@@ -43,8 +43,9 @@ namespace gt
         while (!in.atEnd())
         {
             QString line = in.readLine();
-            builder.parseLine(line.toStdString().c_str(), manager);
+            builder.parseLine(line.toStdString().c_str());
         }
+        builder.complete();
 
         file.close();
         return OpResult::OK;
